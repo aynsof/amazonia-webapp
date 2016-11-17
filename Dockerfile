446f821879a22bd600c5893c48c72ed9d752b0c8
James Kingsmill <james.kingsmill@gmail.com>
2016-11-17 20:51:25 +1100

    Add amazonia resources

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM python:3.5-alpine
 ADD . .
+RUN git clone https://github.com/GeoscienceAustralia/amazonia.git
+RUN cd amazonia
+RUN pip install -e . --user
+RUN cd ..
 RUN pip install -r ./requirements.txt
 ENV PYTHONUNBUFFERED 1
 CMD gunicorn -b 0.0.0.0:8080 -w 4 hello:app --log-file - --access-logfile -
--- hello.py
+++ hello.py
@@ -7,5 +7,18 @@ app = Flask('hello-flask')
 def index():
     return render_template('index.html')
 
+@app.route('/yaml', methods=['POST', 'OPTIONS'])
+def get_cloud_formation():
+
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    text_content = request.get_data(as_text=True)
+    json_content = yaml.safe_load(text_content)
+    default_yaml = amz.read_yaml(os.path.join(__location__, '../amazonia/defaults.yaml'))
+
+    result = amz.generate_template(json_content, default_yaml)
+
+    return Response(result, mimetype='Application/json')
+
 if __name__ == '__main__':
     app.run('localhost')
